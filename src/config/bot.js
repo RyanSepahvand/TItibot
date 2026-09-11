15276d4b60098db1f8f3d8a34a2ee28ea69a10b0
RyanSepahvand <162449148+RyanSepahvand@users.noreply.github.com>
2026-09-11 23:52:16 +0330

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Skyblock Support | /help",     // this is what people actually see
+        state: "GhostHunters Support | /help",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
